0ab45e2582f667c01438b811bd78a6d113f89bac
Apprentice <apprentice@devbootcamp.com>
2013-03-22 15:17:54 -0700

    insert user authenitcation methods to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,25 @@
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+ 
+  has_many :posts
+  has_many :comments
+  has_many :post_votes
+  has_many :comment_votes
+  validates :password_hash, :presence => true
+  include BCrypt
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(pass)
+    @password = Password.create(pass)
+    self.password_hash = @password
+  end
+
+  def self.authenticate(test_user)
+    user = User.find_by_name(test_user['name'])
+    return user if user && (user.password == test_user['password'])
+    nil 
+  end
+
 end
