8d888dedfed78d239575c8085b87fd2bf28f765c
Apprentice <apprentice@devbootcamp.com>
2013-03-22 16:25:06 -0700

    homepage view MVP complete

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,7 +1,20 @@
 get '/' do
-  # Look in app/views/index.erb
   erb :index
 end
 
 post '/signup' do
+  @user = User.new(params[:user])
+  unless @user.save
+    # Errors?
+    erb :index
+  else
+    session[:id] = @user.id
+
+    redirect "/profile"
+  end
 end
+
+# get '/profile' do
+#   @user = User.find(session[:id])
+# end
+
