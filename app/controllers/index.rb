ff168980d494caeead27208dac38c741b06fd341
Apprentice <apprentice@devbootcamp.com>
2013-03-22 16:33:22 -0700

    finish user login/signup/signout, with helper

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,6 +2,21 @@ get '/' do
   erb :index
 end
 
+
+# User Signup/Login
+
+post '/login' do
+  @user = User.find_by_username(params[:user]["email"])
+  if User.authenticate(params[:user])
+     session[:id] = @user.id
+
+     redirect "/profile"
+  else
+     # @error = "You need a proper (and unique) email and password"
+     erb :index
+  end
+end
+
 post '/signup' do
   @user = User.new(params[:user])
   unless @user.save
@@ -12,6 +27,11 @@ post '/signup' do
 
     redirect "/profile"
   end
+end
+
+get '/logout' do
+  session.clear
+  redirect '/'
 end
 
 # get '/profile' do
