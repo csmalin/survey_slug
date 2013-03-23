0ec153dba56ab0a2d09201e146c6bbc864620caa
Apprentice <apprentice@devbootcamp.com>
2013-03-22 18:16:17 -0700

    Settled JS file conflicts

---
+++ public/js/login.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  
+  $('a.login').on('click', function(e){
+    e.preventDefault();
+    
+    if ($('form.login').css("visibility") == "hidden")
+      {
+        $('form.login').css("visibility", "visible");
+      }
+    else
+      {
+        $('form.login').css("visibility", "hidden")
+      }
+  });
+});
