e400bf2b077047f3145b0f759d9a97920da2b8fb
Apprentice <apprentice@devbootcamp.com>
2013-03-22 17:14:43 -0700

    Finish jquery on login form

--- public/js/application.js
+++ public/js/application.js
@@ -1,1 +1,16 @@
-$('form.login').hide();
+$(document).ready(function(){
+  
+  $('a.login').on('click', function(e){
+    e.preventDefault();
+    
+    if ($('form.login').css("visibility") == "hidden")
+      {
+        $('form.login').css("visibility", "visible");
+      }
+    else
+      {
+        $('form.login').css("visibility", "hidden")
+      }
+  });
+
+});
